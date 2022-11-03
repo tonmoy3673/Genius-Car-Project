a51c5a1292382cdaf33822bee45c451079419ad5
tonmoy <tonmoy3673@gmail.com>
2022-11-04 04:34:51 +0600

    set services section

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
+    const [services,setServices]=useState([]);
+        useEffect(()=>{
+            fetch('services.json')
+            .then(res=>res.json())
+            .then(data=>setServices(data))
+
+        },[])
     return (
         <div>
+            <div className='text-center'>
             <p className='text-2xl font-bold text-orange-600'>Services</p>
-            <h2 className='text-5xl font-semibold'>Our Service Area</h2>
+            <h2 className='text-5xl font-semibold py-3'>Our Service Area</h2>
+            <p className='mb-10'>The majority have suffered alteration in some form, by injected humour, or randomised <br/> words which don't look even slightly believable. </p>
+        </div>
+        <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            
+            {
+                services.map(service=><ServiceCard key={service._id} service={service}></ServiceCard>)
+            }
+        </div>
         </div>
     );
 };
